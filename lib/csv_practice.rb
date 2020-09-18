061bdc4154a9ba4ca92f63e2a394031f5039a79c
Ida <ida.goitom@yahoo.com>
2020-09-17 21:13:40 -0700

    practice

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,13 +1,26 @@
 require 'csv'
 require 'awesome_print'
+#you can write anything as the parameter, the test file shows the olympic_file_name
+# is a string that describes the path to the csv file
+def get_all_olympic_athletes(athlete_data)
 
-def get_all_olympic_athletes(filename)
+  olympic_athletes = CSV.read(athlete_data, headers:true). each do |record|
+    ap record
+  end
+  # olympic_athletes = CSV.read(athlete_data)
+  # ap olympic_athletes
 
 end
 
 def total_medals_per_team(olympic_data)
+  olympic_athletes.each do |team|
+    end
 
+  end
 end
 
 def get_all_gold_medalists(olympic_data)
 end
+
+get_all_olympic_athletes('data/athlete_events.csv')
+total_medals_per_team(olympic_data)
